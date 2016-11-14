9cfe8130091e4a146838f1909f493690df9526b3
daichi <daichi@DESKTOP-6QPU3F7>
2016-11-15 01:04:45 +0900

    ボタンの動作追加

--- CustomControlTest/ViewRepairList/repairListWindow.xaml.cs
+++ CustomControlTest/ViewRepairList/repairListWindow.xaml.cs
@@ -42,10 +42,44 @@ namespace ViewRepairList
         private void EquipTypeButton_Clicked(object sender, RoutedEventArgs e)
         {
             var s = (Button)sender;
-            //装備種別チェックボックスの反転
-
+            //装備種別チェックボックスの選択,一括解除
+            var checkBoxes = EquipCheckBox.Children.OfType<CheckBox>().Where(
+                    ch => ch.Uid.Contains(s.Uid));
+            if (checkBoxes.Where(ch => ch.IsChecked == true).Count() == checkBoxes.Count())
+            {
+                foreach (var c in checkBoxes)
+                {
+                    c.IsChecked = false;
+                }
+            }else
+            {
+                foreach (var c in checkBoxes)
+                {
+                    c.IsChecked = true;
+                }
+            }
+            //関連チェックボックス以外のチェックを外す
+            var unSelects = EquipCheckBox.Children.OfType<CheckBox>().Where(
+                    ch => !ch.Uid.Contains(s.Uid));
+            foreach (var b in unSelects)
+            {
+                b.IsChecked = false;
+            }
             //コントロール名で絞り込み条件を減算
             //表示リスト更新
+        }
+
+        private void AllCheck(object sender, RoutedEventArgs e)
+        {
+            //装備と曜日でツリーの深さが違うので一般化できなさそう
+            var ch = (CheckBox)sender;
+            var parent = (WrapPanel)ch.Parent;
+            var checkBoxes = parent.Children.OfType<CheckBox>();
+
+            foreach(var c in checkBoxes)
+            {
+                string str = c.Name;
+            }
         }
 
 
